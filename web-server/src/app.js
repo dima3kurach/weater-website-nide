d97b7d592d6ecec896daea77d729bb36abeac77b
dima3kurach <69084803+dima3kurach@users.noreply.github.com>
2022-09-05 23:40:39 +0300

    Added handlebars package

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,28 @@ const app = express();
 const PORT = 3000;
 const PUBLIC = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(PUBLIC));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    user: 'Admin',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'User message',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
